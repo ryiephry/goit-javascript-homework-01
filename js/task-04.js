ab5d95a3e53d58c721ccf43972101dc665e45483
ryiephry <ephraimpt2@gmail.com>
2023-03-01 17:55:19 -0500

    10

---
+++ js/task-04.js
@@ -0,0 +1,73 @@
+
+const buttonEL =  document.getElementById("counter")
+const sections = {
+    decrement: buttonEL.firstElementChild,
+    increment: buttonEL.lastElementChild,
+    value: document.getElementById("value")
+}
+let counter = 0;
+sections.decrement.addEventListener("click",decrementFunction)
+function decrementFunction() {
+    counter -= 1
+    sections.value.textContent = counter
+}
+
+sections.increment.addEventListener("click", incrementFunction)
+function incrementFunction() {
+    counter += 1
+    sections.value.textContent = counter 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(sections.decrement)
+// const firstButtonEL = document.querySelector("button")
+// console.log(firstButtonEL)
+// const secondButton = document.getElementById("counter")
+// const secondButtonEL = secondButton.lastElementChild
+
+// let decrement = firstButtonEL.textContent
+// let increment = secondButtonEL.textContent
+ 
+// const value = document.getElementById("value")
+
+// const clickFunction = () => {
+//     let numb = 0;
+
+//    value.textContent += decrement
+//     console.log(numb)
+ 
+// }
+// firstButtonEL.addEventListener("click", clickFunction)
