704ca74fb08fd303f9af3f0a2296b5505039bc9b
ryiephry <ephraimpt2@gmail.com>
2023-03-01 23:19:45 -0500

    5 done

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const input = document.getElementById("name-input")
+const output = document.querySelector("h1")
+
+const addingText = (event) => {
+  
+    output.innerHTML= event.currentTarget.value
+};
+
+input.addEventListener("input", addingText)
+
+output.setAttribute("class","outputColor")
