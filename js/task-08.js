b7580137fafa379f5248c74b8a9c76bd57686105
ryiephry <ephraimpt2@gmail.com>
2023-03-02 16:43:52 -0500

    15

--- js/task-08.js
+++ js/task-08.js
@@ -1,16 +1,19 @@
 const form = document.querySelector(".login-form");
+const loginButtonEl = form.lastElementChild
+loginButtonEl.setAttribute("class", "loginButton")
 
 const eventCheck = (event) => {
     event.preventDefault();
-
     const {
         elements: { email, password }
-    } = event.currentTarget;
+          } = event.currentTarget;
 
-    if (email.value === "" || password.value === "") {
-        return console.log("no characters in input section ")
-    } else {
+    if (email.value === "" || password.value === "") { 
+        // return console.log("no characters in input section ");  
+        return alert("either your email or password was not entered")
+} else {
         console.log("your email is:", email.value, "your password is:", password.value)
+        
     }
     form.reset();
     // const storedValues = {email: email.value,password: password.value// }
